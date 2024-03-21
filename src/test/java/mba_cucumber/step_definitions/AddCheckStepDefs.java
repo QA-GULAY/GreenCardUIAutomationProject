3501578e44e3ca1dd36ca56413ce9c171a769da9
SoftSkillSimulasyon <141447378+QA-GULAY@users.noreply.github.com>
2024-03-21 14:20:51 +0100

    Update AddCheckStepDefs.java
    Update AddCheckStepDefs.java

--- src/test/java/mba_cucumber/step_definitions/AddCheckStepDefs.java
+++ src/test/java/mba_cucumber/step_definitions/AddCheckStepDefs.java
@@ -9,40 +9,40 @@ import org.junit.Assert;
 public class AddCheckStepDefs {
     PageAdd pageAdd = new PageAdd();
 
-    @Given("Kullanici beetroot urununu artirir")
+    @Given("User increases beetroot product")
     public void kullanici_beetroot_urununu_artirir() throws InterruptedException {
         pageAdd.BeetrootIncrement();
         pageAdd.BeetrootIncrement();
         Thread.sleep(1000);
     }
 
-    @When("Kullanici beetroot urununu sepete ekler")
+    @When("The user clicks beetroot")
     public void kullanici_beetroot_urununu_sepete_ekler() throws InterruptedException {
         pageAdd.beetrootclick();
         Thread.sleep(1000);
     }
 
-    @When("Kullanici pupkin urununu sepete ekler")
+    @When("The user clicks pumpkin")
     public void kullanici_urununu_sepete_ekler() throws InterruptedException {
         pageAdd.PumpkinClick();
         Thread.sleep(1000);
     }
 
-    @When("Kullanici pomegranate urununu sepete ekler")
+    @When("The user clicks pomegranate")
     public void kullanici_pomegranate_urununu_sepete_ekler() throws InterruptedException {
         pageAdd.PomegranateClick();
         Thread.sleep(1000);
     }
 
 
-    @When("Kullanici sepete gider")
+    @When("The user clicks add to cart")
     public void kullanici_sepete_gider() throws InterruptedException {
         pageAdd.sepeteGitClick();
         Thread.sleep(1000);
 
     }
 
-    @Then("Kullanici urunlerin dogru bir sekilde sepete eklendigini dogrular")
+    @Then("User verifies that the items have been added to the cart correctly")
     public void kullanici_urunlerin_dogru_bir_sekilde_sepete_eklendigini_dogrular() throws InterruptedException {
         String expectedMessagePupkin = "1 No.";
         String expectedMessagePomegranate = "1 No.";
